7b46dfb412b1501b23d9379ee52636c88eee4e31
Fateme Mashayekhi <mashayekhimaral@gmail.com>
2024-09-22 00:34:32 +0330

    style : banner test

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,3 +1,9 @@
 export default function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <div id="banner" className="px-14 py-4">
+        <div className="bg-slate-300 h-[720px]">hii</div>
+      </div>
+    </div>
+  );
 }
