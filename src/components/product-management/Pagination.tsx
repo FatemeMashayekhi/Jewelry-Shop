c03ab58b85770144b4afa1fb575d619fd0efed58
Fateme Mashayekhi <mashayekhimaral@gmail.com>
2024-09-22 21:10:03 +0330

    style : product management created

---
+++ src/components/product-management/Pagination.tsx
@@ -0,0 +1,15 @@
+import { Icon } from "@iconify/react/dist/iconify.js";
+
+export default function Pagination() {
+  return (
+    <div className="join rounded-lg">
+      <button className="join-item btn">
+        <Icon icon="ooui:next-ltr" style={{ color: "black" }} />
+      </button>
+      <button className="join-item btn">صفحه 22</button>
+      <button className="join-item btn">
+        <Icon icon="ooui:next-rtl" style={{ color: "black" }} />
+      </button>
+    </div>
+  );
+}
--- src/pages/ProductsManagement.tsx
+++ src/pages/ProductsManagement.tsx
@@ -1,3 +1,27 @@
+import Pagination from "../components/product-management/Pagination";
+import Table from "../components/product-management/Table";
+
 export default function ProductsManagement() {
-  return <div>ProductsManagement</div>;
+  return (
+    <>
+      <div className="flex flex-col px-14 py-12 gap-y-16">
+        <div className="flex justify-between items-center">
+          <p className="font-semibold">مدیریت کالاها</p>
+          <button className="btn btn-wide rounded-lg bg-green-600 text-white">
+            افزودن کالا
+          </button>
+        </div>
+        <div className="bg-[#102C57] p-10 rounded-2xl">
+          <div className="p-10 rounded-2xl">
+            <div className="bg-[#FEFAF6] p-10 rounded-2xl">
+              <Table />
+              <div className="flex justify-center">
+                <Pagination />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
