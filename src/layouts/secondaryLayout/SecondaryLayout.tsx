073c4efe0594c278a88008f76c218f1cc1f254c4
Fateme Mashayekhi <mashayekhimaral@gmail.com>
2024-09-22 21:27:00 +0330

    style : second layout changed

--- src/layouts/secondaryLayout/SecondaryLayout.tsx
+++ src/layouts/secondaryLayout/SecondaryLayout.tsx
@@ -10,22 +10,22 @@ const menuItems = [
 export default function SecondaryLayout() {
   return (
     <>
-      <div className="flex items-center justify-between px-14 py-10 bg-[#FEFAF6] shadow rounded-3xl">
+      <div className="flex items-center justify-between px-14 py-10 bg-[#FEFAF6] shadow">
         <div className="flex items-center gap-x-2">
           <Icon icon="mdi:rhombus" style={{ color: "black" }} />
           <p className="font-bold text-xl">پنل مدیریت جواهری مشایخی</p>
         </div>
 
         <nav>
-          <menu className="flex gap-x-14 items-center bg-[#EADBC8] p-4 rounded-lg">
+          <menu className="flex gap-x-28 items-center rounded-lg font-bold">
             {menuItems.map((item) => (
               <li key={item.path}>
                 <NavLink
                   to={item.path}
                   className={({ isActive }) =>
                     isActive
-                      ? "text-gray-400 border-b-2 border-gray-400"
-                      : "hover:text-yellow-600"
+                      ? "text-[#55679C] border-b-2 border-[#55679C]"
+                      : "hover:text-[#7C93C3]"
                   }
                 >
                   {item.label}
@@ -35,7 +35,7 @@ export default function SecondaryLayout() {
           </menu>
         </nav>
         <Link to={"/"} className="flex items-center gap-x-2 hover:text-red-500">
-          <p>بازگشت به سایت</p>
+          <p className="font-bold">بازگشت به سایت</p>
           <Icon icon="ic:sharp-arrow-back" style={{ color: "black" }} />
         </Link>
       </div>
