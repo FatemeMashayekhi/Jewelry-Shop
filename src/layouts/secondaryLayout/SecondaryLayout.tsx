59c9805c593b0beb44feab77a3b615de29e4ccc3
Fateme Mashayekhi <mashayekhimaral@gmail.com>
2024-09-22 19:51:54 +0330

    style : second layout created

--- src/layouts/secondaryLayout/SecondaryLayout.tsx
+++ src/layouts/secondaryLayout/SecondaryLayout.tsx
@@ -1,9 +1,44 @@
-import { Outlet } from "react-router-dom";
+import { Icon } from "@iconify/react/dist/iconify.js";
+import { Link, NavLink, Outlet } from "react-router-dom";
+
+const menuItems = [
+  { path: "/management", label: "محصولات" },
+  { path: "/management/inventory", label: "موجودی و قیمت ها" },
+  { path: "/management/orders", label: "سفارش ها" },
+];
 
 export default function SecondaryLayout() {
   return (
     <>
-      <div>SecondLayout</div>
+      <div className="flex items-center justify-between px-14 py-10 bg-[#FEFAF6] shadow rounded-3xl">
+        <div className="flex items-center gap-x-2">
+          <Icon icon="mdi:rhombus" style={{ color: "black" }} />
+          <p className="font-bold text-xl">پنل مدیریت جواهری مشایخی</p>
+        </div>
+
+        <nav>
+          <menu className="flex gap-x-14 items-center bg-[#EADBC8] p-4 rounded-lg">
+            {menuItems.map((item) => (
+              <li key={item.path}>
+                <NavLink
+                  to={item.path}
+                  className={({ isActive }) =>
+                    isActive
+                      ? "text-gray-400 border-b-2 border-gray-400"
+                      : "hover:text-yellow-600"
+                  }
+                >
+                  {item.label}
+                </NavLink>
+              </li>
+            ))}
+          </menu>
+        </nav>
+        <Link to={"/"} className="flex items-center gap-x-2 hover:text-red-500">
+          <p>بازگشت به سایت</p>
+          <Icon icon="ic:sharp-arrow-back" style={{ color: "black" }} />
+        </Link>
+      </div>
       <Outlet />
     </>
   );
