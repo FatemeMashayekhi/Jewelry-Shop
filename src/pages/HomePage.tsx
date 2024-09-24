e4797fa87036c182eadad480b197d6e5ce70bf82
Fateme Mashayekhi <mashayekhimaral@gmail.com>
2024-09-24 15:45:27 +0330

    style : categories section created

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,22 @@
+import { Icon } from "@iconify/react/dist/iconify.js";
+
 export default function HomePage() {
   return (
-    <div>
+    <div className="flex flex-col gap-y-14">
       <div id="banner" className="px-14 py-4 flex justify-center">
         <img src="./src/assets/images/Untitled-1 (2).png" alt="banner-pic" />
+      </div>
+      <div id="categories" className="flex flex-col">
+        <div className="flex justify-center gap-x-3 items-center font-bold text-2xl">
+          <p>دسته بندی</p>
+          <Icon icon="mdi:rhombus" style={{ color: "#bab19e" }} />
+          <p>محصولات</p>
+        </div>
+        <div className="flex justify-center">
+          <p className="text-sm text-gray-400">
+            از بین محصولات متنوع مشایخی انتخاب کنید
+          </p>
+        </div>
       </div>
     </div>
   );
