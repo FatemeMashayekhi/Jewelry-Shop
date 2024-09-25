7e920ea890c6e61791ccf3d25c26e9eba70ee83f
Fateme Mashayekhi <mashayekhimaral@gmail.com>
2024-09-25 15:27:50 +0330

    style : categories row created

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,24 +1,13 @@
-import { Icon } from "@iconify/react/dist/iconify.js";
-import { useContext } from "react";
-import { DataContext } from "../context/context";
+import { Icon } from "@iconify/react";
+import CategoriesCards from "../components/home/CategoriesCards";
 
 export default function HomePage() {
-  const { getAllCategories } = useContext(DataContext);
-  console.log(getAllCategories?.data);
-
-  if (!getAllCategories) {
-    return <div>Loading...</div>;
-  }
-
-  if (getAllCategories.isError) {
-    return <div>Error loading categories</div>;
-  }
   return (
     <div className="flex flex-col gap-y-14">
       <div id="banner" className="px-14 py-4 flex justify-center">
         <img src="./src/assets/images/banner2.png" alt="banner-pic" />
       </div>
-      <div id="categories" className="flex flex-col">
+      <div id="categories" className="flex flex-col gap-y-5">
         <div className="flex justify-center gap-x-3 items-center font-bold text-2xl">
           <p>دسته بندی</p>
           <Icon icon="mdi:rhombus" style={{ color: "#bab19e" }} />
@@ -29,15 +18,7 @@ export default function HomePage() {
             از بین محصولات متنوع مشایخی انتخاب کنید
           </p>
         </div>
-        <div id="categories-cards" className="flex">
-          {Array.isArray(getAllCategories) &&
-            getAllCategories.map((item) => (
-              <div key={item._id}>
-                <img src={item.icon} alt={item._id} />
-                <p>{item.name}</p>
-              </div>
-            ))}
-        </div>
+        <CategoriesCards />
       </div>
     </div>
   );
