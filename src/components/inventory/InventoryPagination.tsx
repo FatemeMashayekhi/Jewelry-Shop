296ad3268cce9df8007a0259fa9808c8641eda7c
Fateme Mashayekhi <mashayekhimaral@gmail.com>
2024-10-07 18:50:46 +0330

    fix : data and pagination set

--- src/components/inventory/InventoryPagination.tsx
+++ src/components/inventory/InventoryPagination.tsx
@@ -1,13 +1,48 @@
-import { Icon } from "@iconify/react/dist/iconify.js";
+import { Icon } from "@iconify/react";
+import { useContext, useEffect } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { DataContext } from "../../context/DataContext";
 
 export default function InventoryPagination() {
+  const { setPage, page, totalPages = 1 } = useContext(DataContext);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const currentPage = parseInt(searchParams.get("page") || "1");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (page !== currentPage.toString()) {
+      setPage?.(currentPage.toString());
+    }
+    if (currentPage > totalPages || currentPage <= 0) {
+      navigate(`/management/inventory/?page=${totalPages}`);
+    }
+  }, [currentPage, setPage, totalPages, page, navigate]);
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      const nextPage = currentPage + 1;
+      setPage?.(nextPage.toString());
+      searchParams.set("page", nextPage.toString());
+      setSearchParams(searchParams);
+    }
+  };
+
+  const handlePrevPage = () => {
+    if (currentPage > 1) {
+      const prevPage = currentPage - 1;
+      setPage?.(prevPage.toString());
+      searchParams.set("page", prevPage.toString());
+      setSearchParams(searchParams);
+    }
+  };
+
   return (
     <div className="join rounded-lg">
-      <button className="join-item btn">
+      <button className="join-item btn" onClick={handleNextPage}>
         <Icon icon="ooui:next-ltr" style={{ color: "black" }} />
       </button>
-      <button className="join-item btn">صفحه 22</button>
-      <button className="join-item btn">
+      <button className="join-item btn">صفحه {currentPage}</button>
+      <button className="join-item btn" onClick={handlePrevPage}>
         <Icon icon="ooui:next-rtl" style={{ color: "black" }} />
       </button>
     </div>
