bc05f9b5c8861e2419904ab94641d8037b0fb696
Fateme Mashayekhi <mashayekhimaral@gmail.com>
2024-10-14 17:52:46 +0330

    bug : offer section scroll

--- src/components/home/OfferSection.tsx
+++ src/components/home/OfferSection.tsx
@@ -1,3 +1,4 @@
+import React, { useRef } from "react";
 import { useContext } from "react";
 import { DataContext } from "../../context/DataContext";
 import { ProductsEntity } from "../../models/GetProductsModel";
@@ -5,14 +6,51 @@ import CardComponent from "../card/Card";
 
 const OfferSection: React.FC = () => {
   const { getDiscountProducts } = useContext(DataContext);
+  const containerRef = useRef<HTMLDivElement | null>(null);
+
+  const handleMouseDown = (e: React.MouseEvent) => {
+    const container = containerRef.current;
+    if (!container) return;
+
+    container.classList.add("cursor-grabbing");
+    container.classList.remove("cursor-grab");
+
+    const startX = e.pageX - container.offsetLeft;
+    const scrollLeft = container.scrollLeft;
+
+    const handleMouseMove = (e: MouseEvent) => {
+      const x = e.pageX - container.offsetLeft;
+      const walk = x - startX; // Calculate the distance to scroll
+      container.scrollLeft = scrollLeft - walk;
+    };
+
+    const handleMouseUp = () => {
+      container.classList.remove("cursor-grabbing");
+      container.classList.add("cursor-grab");
+      document.removeEventListener("mousemove", handleMouseMove);
+      document.removeEventListener("mouseup", handleMouseUp);
+    };
+
+    document.addEventListener("mousemove", handleMouseMove);
+    document.addEventListener("mouseup", handleMouseUp);
+  };
 
   return (
-    <div id="offer" className="flex px-20 gap-x-14 justify-center items-center">
-      <div id="img">
+    <div
+      id="offer"
+      className="flex px-20 gap-x-14 justify-center items-center overflow-hidden"
+    >
+      {/* <div id="img">
         <img src="./src/assets/images/offer-pic.png" alt="offer-pic" />
-      </div>
-      <div id="offer-products" className="flex items-center gap-y-4">
-        <div id="cards-container" className="flex gap-x-10">
+      </div> */}
+      <div id="offer-products" className="flex items-center gap-y-4 flex-1">
+        <div
+          id="cards-container"
+          className="flex gap-x-10 no-scrollbar overflow-x-auto cursor-grab py-6 w-full"
+          onMouseDown={handleMouseDown}
+          ref={containerRef}
+          style={{ flex: "0 0 auto", display: "flex", alignItems: "center" }}
+        >
           {Array.isArray(getDiscountProducts?.data) &&
             getDiscountProducts?.data.map((item: ProductsEntity) => (
               <CardComponent key={item._id} item={item} />
