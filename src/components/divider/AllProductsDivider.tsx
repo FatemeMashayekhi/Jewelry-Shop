7aee5b2882bb70b62edb709ec39e53ec6942e655
Fateme Mashayekhi <mashayekhimaral@gmail.com>
2024-10-15 19:51:35 +0330

    style : dividers added to home page

--- src/components/divider/AllProductsDivider.tsx
+++ src/components/divider/AllProductsDivider.tsx
@@ -1,14 +1,7 @@
-import { Icon } from "@iconify/react";
-
 export default function AllProductsDivider() {
   return (
-    <div
-      id="divider"
-      className="bg-[#e5dfd7] flex justify-center items-center gap-x-4 p-2"
-    >
-      <Icon icon="mdi:rhombus" style={{ color: "#bab19e" }} />
-      <p className="text-xl">همه ی محصولات</p>
-      <Icon icon="mdi:rhombus" style={{ color: "#bab19e" }} />
+    <div className="divider p-2 text-xl font-semibold text-[#494949]">
+      همه ی محصولات
     </div>
   );
 }
---
+++ src/components/divider/DiscountDivider.tsx
@@ -0,0 +1,5 @@
+export default function DiscountDivider() {
+  return (
+    <div className="divider text-xl font-semibold text-[#494949]">تخفیف ها</div>
+  );
+}
